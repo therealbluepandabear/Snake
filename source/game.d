89d3d2392a617df9d5b3a53f918d2403c0343ba6
therealbluepandabear <justaguyoney@gmail.com>
2022-12-15 16:38:29 +1300

    Add some code to `Game` class, fix some code for `Snake` class.

--- source/game.d
+++ source/game.d
@@ -7,10 +7,26 @@ import window;
 
 class Game {
     this() {
-        window = new Window("Snake", sfVector2u(800, 600));
+        m_window = new Window("Snake", sfVector2u(800, 600));
+
+        if (sfKeyboard_isKeyPressed(sfKeyCode.sfKeyUp) && (m_snake.dir != Direction.down)) {
+            m_snake.dir = Direction.up;
+        } else if (sfKeyboard_isKeyPressed(sfKeyCode.sfKeyDown) && (m_snake.dir != Direction.up)) {
+            m_snake.dir = Direction.down;
+        } else if (sfKeyboard_isKeyPressed(sfKeyCode.sfKeyLeft) && (m_snake.dir != Direction.right)) {
+            m_snake.dir = Direction.left;
+        } else if (sfKeyboard_isKeyPressed(sfKeyCode.sfKeyRight) && (m_snake.dir != Direction.left)) {
+            m_snake.dir = Direction.right;
+        }
     }
+
+    void update() {
+
+    }
+
 private:
-    World world;
-    Snake snake;
-    Window window;
+    World m_world;
+    Snake m_snake;
+    Window m_window;
+    sfTime m_elapsed;
 }
--- source/snake.d
+++ source/snake.d
@@ -19,120 +19,120 @@ alias SnakeContainer = SnakeSegment[];
 
 class Snake {
     this(int size) {
-        this.size = size;
-        bodyRect.sfRectangleShape_setSize(sfVector2f(size - 1, size - 1));
+        this.m_size = size;
+        m_bodyRect.sfRectangleShape_setSize(sfVector2f(size - 1, size - 1));
         reset();
     }
 
-    void setDirection(Direction dir) {
-        this.dir = dir;
+    void dir(Direction dir) {
+        this.m_dir = dir;
     }
 
-    Direction getDirection() {
-        return dir;
+    Direction dir() {
+        return m_dir;
     }
 
-    int getSpeed() {
-        return speed;
+    int speed() {
+        return m_speed;
     }
 
-    sfVector2i getPosition() {
-        return (!(snakeBody.length == 0)) ? snakeBody[0].position : sfVector2i(1, 1);
+    sfVector2i pos() {
+        return (!(m_snakeBody.length == 0)) ? m_snakeBody[0].position : sfVector2i(1, 1);
     }
 
-    int getLives() {
+    int lives() {
         return lives;
     }
 
-    int getScore() {
+    int score() {
         return score;
     }
 
-    void increaseScore() {
-        score += 10;
+    void incScore() {
+        m_score += 10;
     }
 
-    bool hasLost() {
+    bool lost() {
         return lost;
     }
 
     void lose() {
-        this.lost = true;
+        this.m_lost = true;
     }
 
     void toggleLost() {
-        this.lost = !lost;
+        this.m_lost = !m_lost;
     }
 
     void extend() {
-        if (snakeBody.length == 0) {
+        if (m_snakeBody.length == 0) {
             return;
         }
 
-        SnakeSegment tailHead = snakeBody[snakeBody.length - 1];
+        SnakeSegment tailHead = m_snakeBody[m_snakeBody.length - 1];
 
-        if (snakeBody.length > 1) {
-            SnakeSegment tailBone = snakeBody[snakeBody.length - 2];
+        if (m_snakeBody.length > 1) {
+            SnakeSegment tailBone = m_snakeBody[m_snakeBody.length - 2];
 
             if (tailHead.position.x == tailBone.position.x) {
                 if (tailHead.position.y > tailBone.position.y) {
-                    snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y + 1);
+                    m_snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y + 1);
                 } else {
-                    snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y - 1);
+                    m_snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y - 1);
                 }
             } else if (tailHead.position.y == tailBone.position.y) {
                 if (tailHead.position.x > tailBone.position.x) {
-                    snakeBody ~= SnakeSegment(tailHead.position.x + 1, tailHead.position.y);
+                    m_snakeBody ~= SnakeSegment(tailHead.position.x + 1, tailHead.position.y);
                 } else {
-                    snakeBody ~= SnakeSegment(tailHead.position.x - 1, tailHead.position.y);
+                    m_snakeBody ~= SnakeSegment(tailHead.position.x - 1, tailHead.position.y);
                 }
             }
         } else {
             if (dir == Direction.up) {
-                snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y + 1);
+                m_snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y + 1);
             } else if (dir == Direction.down) {
-                snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y - 1);
+                m_snakeBody ~= SnakeSegment(tailHead.position.x, tailHead.position.y - 1);
             } else if (dir == Direction.left) {
-                snakeBody ~= SnakeSegment(tailHead.position.x + 1, tailHead.position.y);
+                m_snakeBody ~= SnakeSegment(tailHead.position.x + 1, tailHead.position.y);
             } else if (dir == Direction.right) {
-                snakeBody ~= SnakeSegment(tailHead.position.x - 1, tailHead.position.y);
+                m_snakeBody ~= SnakeSegment(tailHead.position.x - 1, tailHead.position.y);
             }
         }
     }
 
     void reset() {
-        snakeBody = SnakeContainer.init;
+        m_snakeBody = SnakeContainer.init;
 
-        snakeBody ~= SnakeSegment(5, 7);
-        snakeBody ~= SnakeSegment(5, 6);
-        snakeBody ~= SnakeSegment(5, 5);
+        m_snakeBody ~= SnakeSegment(5, 7);
+        m_snakeBody ~= SnakeSegment(5, 6);
+        m_snakeBody ~= SnakeSegment(5, 5);
 
-        setDirection(Direction.none);
+        m_dir = Direction.none;
 
-        this.speed = 15;
-        this.lives = 3;
-        this.score = 0;
-        this.lost = false;
+        this.m_speed = 15;
+        this.m_lives = 3;
+        this.m_score = 0;
+        this.m_lost = false;
     }
 
     void move() {
-        for (int i = cast(int)(snakeBody.length - 1); i > 0; --i) {
-            snakeBody[i].position = snakeBody[i - 1].position;
+        for (int i = cast(int)(m_snakeBody.length - 1); i > 0; --i) {
+            m_snakeBody[i].position = m_snakeBody[i - 1].position;
         }
 
         if (dir == Direction.left) {
-            --snakeBody[0].position.x;
+            --m_snakeBody[0].position.x;
         } else if (dir == Direction.right) {
-            ++snakeBody[0].position.x;
+            ++m_snakeBody[0].position.x;
         } else if (dir == Direction.up) {
-            --snakeBody[0].position.y;
+            --m_snakeBody[0].position.y;
         } else if (dir == Direction.down) {
-            ++snakeBody[0].position.y;
+            ++m_snakeBody[0].position.y;
         }
     }
 
     void tick() {
-        if (snakeBody.length == 0) {
+        if (m_snakeBody.length == 0) {
             return;
         }
 
@@ -149,47 +149,47 @@ class Snake {
     }
 
     void render(sfRenderWindow* window) {
-        if (snakeBody.length == 0) {
+        if (m_snakeBody.length == 0) {
             return;
         }
 
-        SnakeSegment head = snakeBody[0];
+        SnakeSegment head = m_snakeBody[0];
 
-        bodyRect.sfRectangleShape_setFillColor(sfYellow);
-        bodyRect.sfRectangleShape_setPosition(sfVector2f(head.position.x * size, head.position.y * size));
-        window.sfRenderWindow_drawRectangleShape(bodyRect, null);
+        m_bodyRect.sfRectangleShape_setFillColor(sfYellow);
+        m_bodyRect.sfRectangleShape_setPosition(sfVector2f(head.position.x * m_size, head.position.y * m_size));
+        window.sfRenderWindow_drawRectangleShape(m_bodyRect, null);
 
-        bodyRect.sfRectangleShape_setFillColor(sfGreen);
+        m_bodyRect.sfRectangleShape_setFillColor(sfGreen);
 
-        for (int i = 1; i < snakeBody.length; ++i) {
-            bodyRect.sfRectangleShape_setPosition(sfVector2f(snakeBody[i].position.x * size, snakeBody[i].position.y * size));
-            window.sfRenderWindow_drawRectangleShape(bodyRect, null);
+        for (int i = 1; i < m_snakeBody.length; ++i) {
+            m_bodyRect.sfRectangleShape_setPosition(sfVector2f(m_snakeBody[i].position.x * m_size, m_snakeBody[i].position.y * m_size));
+            window.sfRenderWindow_drawRectangleShape(m_bodyRect, null);
         }
     }
 
 private:
     void checkCollision() {
-        if (snakeBody.length < 5) {
+        if (m_snakeBody.length < 5) {
             return;
         }
 
-        SnakeSegment head = snakeBody[0];
+        SnakeSegment head = m_snakeBody[0];
 
-        for (int i = 1; i < snakeBody.length; ++i) {
-            if (snakeBody[i].position == head.position) {
-                int segments = cast(int)(snakeBody.length) - i;
+        for (int i = 1; i < m_snakeBody.length; ++i) {
+            if (m_snakeBody[i].position == head.position) {
+                int segments = cast(int)(m_snakeBody.length) - i;
                 cut(segments);
                 break;
             }
         }
     }
 
-    SnakeContainer snakeBody;
-    int size;
-    Direction dir;
-    int speed;
-    int lives;
-    int score;
-    bool lost;
-    sfRectangleShape* bodyRect;
+    SnakeContainer m_snakeBody;
+    int m_size;
+    Direction m_dir;
+    int m_speed;
+    int m_lives;
+    int m_score;
+    bool m_lost;
+    sfRectangleShape* m_bodyRect;
 }
