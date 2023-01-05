e39625ff0a44d01adc139f344871b3fd5d0faab6
therealbluepandabear <justaguyoney@gmail.com>
2023-01-06 11:59:06 +1300

    Add 50x50 board size.

--- source/gamesettings.d
+++ source/gamesettings.d
@@ -5,7 +5,8 @@ import std.typecons;
 enum BoardSize : Tuple!(int, int) {
     small = tuple(5, 5),
     medium = tuple(20, 20),
-    large = tuple(35, 35)
+    large = tuple(35, 35),
+    very_large = tuple(50, 50)
 }
 
 struct GameSettings {
