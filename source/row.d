326413eb5eb97ea897ff141d9fcc2b3690b0b9ae
therealbluepandabear <justaguyoney@gmail.com>
2023-01-06 14:18:43 +1300

    Remove unused `_cursorOffset` parameter.

--- source/row.d
+++ source/row.d
@@ -17,8 +17,8 @@ class Row(T) {
         }
     }
 
-    void addChild(T child, float _cursorOffset = 0) {
-        child.sfDrawableExt_setPosition(sfVector2f(_cursor + _cursorOffset + _position.x, _position.y));
+    void addChild(T child) {
+        child.sfDrawableExt_setPosition(sfVector2f(_cursor + _position.x, _position.y));
         _cursor += child.sfDrawableExt_getSize().x;
         _children ~= child;
     }
